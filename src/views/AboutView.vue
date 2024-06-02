7b0681ca35bdc423b47339eb98db9a3b88fdb4cb
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-06-02 08:52:08 +0200

    change text on homeview and aboutview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <!-- <base-button /> -->
+  <div class="about p-4">
+    <h1 class="text-3xl">Prototype music web application</h1>
+    <br />
+    <p>
+      This website is built during a Vue.js online course by Zero-to-Mastery and is used as a
+      training environment for practicing Vue.js and other web technologies.
+    </p>
+    <br />
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptatem impedit,
+      nobis officiis odit rem voluptatum cumque sed magni unde beatae similique sapiente corporis
+      deserunt commodi qui quis ipsa! Ipsum.
+    </p>
   </div>
 </template>
